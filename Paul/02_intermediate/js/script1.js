c2bb972a68a46e7ca1d1bb3ceec19830b36fe217
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-05-27 15:14:06 +0200

    advanced exercises

---
+++ Paul/02_intermediate/js/script1.js
@@ -0,0 +1,10 @@
+
+capitalize("i am a web developer");
+
+function capitalize (lowcasestring){
+	firstletter = lowcasestring.charAt().toUpperCase();
+	lowcasestring = lowcasestring.slice(1,);
+	upcasestring = firstletter + lowcasestring;
+
+	document.write(upcasestring);
+}
